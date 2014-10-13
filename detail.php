54efffe0b5b8bbb4b7ec1c536144f7a6d2f4be53
Anastasiia <Anastasiia@192.168.1.52>
2014-10-13 18:33:50 +0300

    add 'back to list' link to bottom of the detail page

--- detail.php
+++ detail.php
@@ -4,10 +4,10 @@ use Model\View, Model\SessionService;
 
 $sessionService = new SessionService();
 require_once 'templates/header.html';
-require_once 'templates/backToList.html';
+require 'templates/backToList.html';
 
 $view = new View;
 $result = $view->getDetail();
 
-require_once 'templates/backToList.html';
+require 'templates/backToList.html';
 require_once 'templates/footer.html';
